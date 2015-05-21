68a6014b96f99ab276ed436d20b152dc69707c96
amiguel <alejandro.miguel.herrero@gmail.com>
2015-05-21 17:19:39 +0200

    Getter and setters

--- src/main/java/com/ethertion/jft/pojo/Book.java
+++ src/main/java/com/ethertion/jft/pojo/Book.java
@@ -6,8 +6,9 @@ package com.ethertion.jft.pojo;
  */
 public class Book implements Document {
     
-    private final String text = "This is the story of ...";
-    
+    private final String text = "This is the story of ...";    
+    private final String title = "The Neverending Story";
+        
     public Book(){
         
     }
@@ -16,6 +17,19 @@ public class Book implements Document {
     public void read(int n) {
         String sub = text.substring(0, n-1);
         System.out.println(sub);
+    }
+    
+    @Override
+    public String toString() {
+        return "Book{" + "text=" + text + ", title=" + title + '}';
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getTitle() {
+        return title;
     }
     
 }
