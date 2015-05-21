48180328266869b6efbd857acbd39a45308ab8e1
amiguel <alejandro.miguel.herrero@gmail.com>
2015-05-21 17:18:23 +0200

    Predicates and functions

--- src/main/java/com/ethertion/jft/Lambda.java
+++ src/main/java/com/ethertion/jft/Lambda.java
@@ -4,25 +4,28 @@ import com.ethertion.jft.pojo.Book;
 import com.ethertion.jft.pojo.Document;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  *
  * @author amiguel
  */
 public class Lambda {
-           
-    public void test (){
-    
+
+    public void test() {
+
         //foreach
         //old way
-        List<String> list = Arrays.asList("a", "b");
-        for (String s:list){
-            System.out.println (s);
+        List<String> letters = Arrays.asList("a", "b");
+        for (String s : letters) {
+            System.out.println(s);
         }
         //new way
-        list.forEach(s -> System.out.println (s));        
+        letters.forEach(s -> System.out.println(s));
         System.out.println("-----");
-        
+
         //functional interface
         //old implementes a subclass of the interface
         Document book = new Book();
@@ -31,11 +34,54 @@ public class Lambda {
         book = (n) -> (System.out.println("This is the story of ...".substring(0, n-1)));       
         book.read(5);        
         System.out.println("-----");
-        
+
         //interface with default method
         book.identify();
-        System.out.println("-----");        
+        System.out.println("-----");
+
+        //predicates
+        List books = Arrays.asList(new Book());
+        this.printBook(books, b -> b.getTitle().equals("The Neverending Story"));
+        System.out.println("-----");
         
+        Predicate<Integer> isOdd = n -> n % 2 != 0;
+        List<Integer> numbers = Arrays.asList(2, 3, 4, 5, new Random().nextInt());
+        this.printNumber(numbers, isOdd);
+        System.out.println("-----");
+        
+        //functions
+        Function<Integer, Integer> add1 = x -> x + 1;
+        Integer r = add1.apply(1);
+        System.out.println(r);
+        System.out.println("-----");
+
+    }
+
+    /**
+     * Prints a book if the predicate is true.
+     *
+     * @param list
+     * @param tester
+     */
+    private void printBook(List<Book> list, Predicate<Book> tester) {
+        for (Book b : list) {
+            if (tester.test(b)) {
+                System.out.println(b.toString());
+            }
+        }
     }
     
+    /**
+     * Prints a number if the predicate is true.
+     * @param list
+     * @param tester 
+     */
+    private void printNumber (List<Integer> list, Predicate<Integer> tester) {
+        for (Integer e : list) {
+            if (tester.test(e)) {
+                System.out.println(e.toString());
+            }
+        }
+    }
+
 }
--- src/main/java/com/ethertion/jft/Stream.java
+++ src/main/java/com/ethertion/jft/Stream.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.ethertion.jft;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  *
@@ -14,13 +11,18 @@ import java.util.List;
  */
 public class Stream {
     
-    public void test(){
-        
+    public void test (){
+    
         //foreach ---        
-        List<String> list = Arrays.asList("a", "b");       
-        System.out.println(list.stream().count());
+        List<String> letters = Arrays.asList("a", "b");       
+        System.out.println(letters.stream().count());
         System.out.println("-----");
         
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5); 
+        Predicate<Integer> isOdd = n -> n % 2 != 0;       
+        //Stream<Integer> stream = numbers.stream().filter(isOdd);
+        System.out.println("-----"); 
+               
     }
     
 }
